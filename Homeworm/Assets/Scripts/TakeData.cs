bdf904e8778c65e9850ed87d863c566faa1e8272
Kakaroto9000 <oleksandr.klindukh@gmail.com>
2021-11-11 20:29:08 +0200

    Finish

--- Homeworm/Assets/Scripts/TakeData.cs
+++ Homeworm/Assets/Scripts/TakeData.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TakeData : MonoBehaviour
 {
-    public TankParametrs TakenData;
-    public void OnMouseDown()
+    public Text nameTank;
+    public Text damageTank;
+    public Text tankSpeed;
+    public Text speedRotahion;
+    public Text reloadTime;
+    public Text cost;
+    public void OnMoused(TankParametrs TakenData)
     {
-        Debug.Log(TakenData.TankName); 
-        Debug.Log(TakenData.Damage);
-        Debug.Log(TakenData.Speed);
-        Debug.Log(TakenData.Rotationspeed);
-        Debug.Log(TakenData.Reload);
-        Debug.Log(TakenData.cost);
+        nameTank.text = string.Format(TakenData.TankName);
+        damageTank.text = string.Format(TakenData.Damage);
+        tankSpeed.text = string.Format(TakenData.Speed);
+        speedRotahion.text = string.Format(TakenData.Rotationspeed);
+        reloadTime.text = string.Format(TakenData.Reload);
+        cost.text = string.Format(TakenData.cost);
     }
 }
